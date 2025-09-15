cabd77f3c65bfa525fe6c80ff252c727b1e50b35
yulyuri <tanchyn2@gmail.com>
2025-09-15 13:08:23 +0800

    working i2c3 and i2c5

--- M7/evkmimx8mp_ii2c_polling_b2b_transfer_master_cm7/app.h
+++ M7/evkmimx8mp_ii2c_polling_b2b_transfer_master_cm7/app.h
@@ -10,18 +10,25 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-/*${macro:start}*/
-#define EXAMPLE_I2C_MASTER_BASEADDR I2C5
-#define I2C_MASTER_CLK_FREQ                                                                 \
+/* I2C5 (MPU6050) */
+#define I2C5_MASTER_BASE        I2C5
+#define I2C5_MASTER_CLK_FREQ                                                               \
     (CLOCK_GetPllFreq(kCLOCK_SystemPll1Ctrl) / (CLOCK_GetRootPreDivider(kCLOCK_RootI2c5)) / \
      (CLOCK_GetRootPostDivider(kCLOCK_RootI2c5)) / 5) /* SYSTEM PLL1 DIV5 */
-/*${macro:end}*/
+
+/* I2C3 (ADXL345) */
+#define I2C3_MASTER_BASE        I2C3
+#define I2C3_MASTER_CLK_FREQ                                                               \
+    (CLOCK_GetPllFreq(kCLOCK_SystemPll1Ctrl) / (CLOCK_GetRootPreDivider(kCLOCK_RootI2c3)) / \
+     (CLOCK_GetRootPostDivider(kCLOCK_RootI2c3)) / 5) /* SYSTEM PLL1 DIV5 */
+
+/* Back-compat: your existing code uses these. Keep them pointing at I2C5. */
+#define EXAMPLE_I2C_MASTER_BASEADDR I2C5_MASTER_BASE
+#define I2C_MASTER_CLK_FREQ         I2C5_MASTER_CLK_FREQ
 
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
-/*${prototype:start}*/
 void BOARD_InitHardware(void);
-/*${prototype:end}*/
 
 #endif /* _APP_H_ */
